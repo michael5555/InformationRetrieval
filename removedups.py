64a3a3b6ee23d283004c3758f7fbeceab9535b14
Michael Adriaensen <michaeladriaensen@gmail.com>
2018-11-22 23:08:07 +0100

    removedups fixed pandas needed

--- removedups.py
+++ removedups.py
@@ -1,6 +1,5 @@
-from more_itertools import unique_everseen
 import sys
-
+import pandas as pd
 
 if __name__ == '__main__':
 
@@ -9,6 +8,6 @@ if __name__ == '__main__':
         readfile = sys.argv[1]
         outfile = sys.argv[2]
 
-
-        with open(readfile,'r') as f, open(outfile,'w') as nodups:
-            nodups.writelines(unique_everseen(f))
+        toclean = pd.read_csv(readfile,header=None,names=["id","idfloatminusone","text","redditid","subreddit","metareddit","time","author","ups","downs","authorlinkkarma","authorkarma","authorisgold"])
+        deduped = toclean.drop_duplicates(["text","author"])
+        deduped.to_csv(outfile)

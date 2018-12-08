bfc56ae66e450d015fba88428915e3ea3918774f
Michael Adriaensen <michaeladriaensen@gmail.com>
2018-12-08 23:51:49 +0100

    added a version of pseudo relevance feedback to gutenbergquerier.py. still problems.

--- RelevanceFeedback.py
+++ RelevanceFeedback.py
@@ -1,11 +1,12 @@
 import os, sys, lucene
+from math import log
 
 from java.nio.file import Paths
 from java.lang import System
 from java.text import DecimalFormat
 from java.util import Arrays
 
-from org.apache.lucene.search import IndexSearcher, TermQuery, MatchAllDocsQuery, BoostQuery
+from org.apache.lucene.search import IndexSearcher, TermQuery, MatchAllDocsQuery,BooleanQuery,BooleanClause
 from org.apache.lucene.store import FSDirectory, SimpleFSDirectory
 from org.apache.lucene.index import (IndexWriter, IndexReader,
                                      DirectoryReader, Term, Terms,
@@ -15,8 +16,9 @@ from org.apache.lucene.analysis.miscellaneous import LimitTokenCountAnalyzer
 from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.document import Document
+from org.apache.lucene.queries.mlt import MoreLikeThis
 
-class RelevanceFeedback(object):
+"""class RelevanceFeedback(object):
 
     def __init__(self,a,b,c,query,result,reader):
         self.alpha = a
@@ -27,7 +29,7 @@ class RelevanceFeedback(object):
         (self.relevant,self.nonrelevant) = self.splitTopDocs(result,reader)
 
     def splitTopDocs(self,result, reader):
-        """ get the relevant and non relevant docs"""
+        
         relevant = []
         nonrelevant = []
         ixreader = IndexSearcher(reader)
@@ -47,33 +49,82 @@ class RelevanceFeedback(object):
     def calculateSumRelevant(self,reader):
         ixreader = IndexSearcher(reader)
         alldocstermsfreqs = []
+        totalidfvec = []
         for i in self.relevant:
+            idfvec = []
             doctermsfreqs = []
-            terms = ixreader.getTermVector(i.id,"content")
+            terms = ixreader.getTermVector(i,"content")
             itr = terms.iterator(0)
             while itr.next():
                 post = itr.postings()
-                freq = post.freq()
-                doctermsfreqs.append(freq)
+                tf = post.freq() ** 0.5
+                if tf > 0:
+                    idfvec.append(1)
+                else:
+                    idfvec.append(0)
+                doctermsfreqs.append(tf)
             alldocstermsfreqs.append(doctermsfreqs)
+            totalidfvec.append(idfvec)
+
+        totalidfvec = [sum(x) for x in zip(*totalidfvec)]
+
+        for i in totalidfvec:
+            i = float(1 + log(4/i + 1))
+
 
 
 
     def calculateSumNonRelevant(self,reader):
         ixreader = IndexSearcher(reader)
         alldocstermsfreqs = []
+        totalidfvec = []
         for i in self.nonrelevant:
+            idfvec = []
             doctermsfreqs = []
-            terms = ixreader.getTermVector(i.id,"content")
+            terms = ixreader.getTermVector(i,"content")
             itr = terms.iterator(0)
             while itr.next():
                 post = itr.postings()
-                freq = post.freq()
-                doctermsfreqs.append(freq)
+                tf = post.freq() ** 0.5
+                if tf > 0:
+                    idfvec.append(1)
+                else:
+                    idfvec.append(0)
+                doctermsfreqs.append(tf)
             alldocstermsfreqs.append(doctermsfreqs)
+            totalidfvec.append(idfvec)
 
-    def expandQuery(self):
-        pass
+        totalidfvec = [sum(x) for x in zip(*totalidfvec)]
+
+        for i in totalidfvec:
+            i = float(1 + log(8/i + 1))
+
+
+        tfidffreqvectors = []
+        for it in alldocstermsfreqs:
+            itvector = []
+            for index in range(len(it)):
+                tfidf = it[index] * totalidfvec[index]
+                itvector.append(tfidf) """
+
+
+
+
+
+def expandQuery(ixreader,result,nrRelevant):
+    relevant = []
+    mlt = MoreLikeThis(ixreader)
+    for i in result[0:nrRelevant - 1]:
+        docid = ixreader.doc(i.doc)
+        relevant.append(mlt.like(docid))
+
+    querybuilder = BooleanQuery.Builder()
+    for i in relevant:
+        querybuilder.add(i,BooleanClause.Occur.SHOULD)
+
+    return querybuilder.build()
+        
+
 
 
 
--- gutenbergquerier.py
+++ gutenbergquerier.py
@@ -5,7 +5,7 @@ from java.lang import System
 from java.text import DecimalFormat
 from java.util import Arrays
 
-from org.apache.lucene.search import IndexSearcher, TermQuery, MatchAllDocsQuery
+from org.apache.lucene.search import IndexSearcher, TermQuery, MatchAllDocsQuery,BooleanQuery,BooleanClause
 from org.apache.lucene.store import FSDirectory, SimpleFSDirectory
 from org.apache.lucene.index import (IndexWriter, IndexReader,
                                      DirectoryReader, Term,
@@ -15,6 +15,8 @@ from org.apache.lucene.analysis.miscellaneous import LimitTokenCountAnalyzer
 from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.document import Document
+from org.apache.lucene.queries.mlt import MoreLikeThis
+
 
 TEXT = "text"
 
@@ -49,7 +51,24 @@ class Querrier(object):
 
         return scoreDocs
 
-		
+    def expandQuery(self,result,nrRelevant):
+        searcher = IndexSearcher(self.reader)
+     
+        relevant = []
+        mlt = MoreLikeThis(self.reader)
+        for i in result[0:nrRelevant - 1]:
+            docid = self.reader.doc(i.doc)
+            relevant.append(mlt.like(docid))
+
+        querybuilder = BooleanQuery.Builder()
+        for i in relevant:
+            querybuilder.add(i,BooleanClause.Occur.SHOULD)
+
+        combinedquery = querybuilder.build()
+        scoreDocs = searcher.search(combinedquery, 10).scoreDocs
+        return scoreDocs
+
+
 if __name__ == '__main__':
     lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     searcher = Querrier(os.path.dirname(os.path.abspath(sys.argv[0])))
@@ -75,6 +94,32 @@ if __name__ == '__main__':
             score = res.score
 
             print("title: {0}author: {1}score: {2}\n".format(title,author,score))
+
+        print("Do you want to expand your query? [Y/n] (enter empty string to exit):")
+        expandval = input()
+
+        if expandval == "":
+            # Break the loop
+            print("The querrier will shut down.")
+            break
+        elif expandval == "n" or "no" or "N" or "NO":
+            continue
+
+        elif expandval == "y" or "Y" or "Yes" or "YES":
+
+            # expand query and output results
+            expresult = searcher.expandQuery(result,3)
+            for res in expresult:
+                ixreader = IndexSearcher(searcher.reader)
+                doc = ixreader.doc(res.doc)
+                text = doc.get("content")
+                author = doc.get("author")
+                title = doc.get("title")
+                score = res.score
+
+                print("title: {0}author: {1}score: {2}\n".format(title,author,score))
+            
+
 
 
 

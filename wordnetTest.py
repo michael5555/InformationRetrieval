4044012c10a003555a985f03481c7df8f05740eb
Ken-B1 <kenbauwens@hotmail.com>
2018-12-11 15:35:13 +0100

    Wordnet for synonym searching

---
+++ wordnetTest.py
@@ -0,0 +1,88 @@
+import lucene
+from java.io import StringReader, Reader
+from org.apache.lucene.analysis.synonym import WordnetSynonymParser
+
+from org.apache.lucene.analysis.core import WhitespaceAnalyzer,  FlattenGraphFilter
+from org.apache.lucene.analysis.standard import StandardTokenizer
+from org.apache.lucene.analysis.tokenattributes import CharTermAttribute
+from org.apache.lucene.analysis.synonym import SynonymGraphFilter, SynonymMap
+from org.apache.lucene.util import CharsRef
+
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+
+class Synonyms:
+
+    def __init__(self, analyser=WhitespaceAnalyzer(), file="prolog/wn_s.pl"):
+        self.analyser = analyser
+        self.file = file
+
+        self.parser = WordnetSynonymParser(True, True, analyser)
+
+        # Read the prolog-file for wordnet in a stringreader
+        PlFile = StringReader(open("prolog/wn_s.pl", 'r').read())
+
+        # Parse the prologfile with the WordnetSynonymParser
+        self.parser.parse(PlFile)
+
+        # Build the synonymmap
+        self.map = self.parser.build()
+
+    def getSynonyms(self, query, tokenizer=StandardTokenizer()):
+        '''
+        :param query: The query for which to get synonyms
+        :param tokenizer: The tokenizer used for synonymgraphfilter
+        :return: A tokenStream with the synonyms
+        '''
+
+        tokenizer.setReader(StringReader(query))
+        a = SynonymGraphFilter(tokenizer, self.map, True)
+        returnvalue = FlattenGraphFilter(a)
+        tokenizer.close()
+        return returnvalue
+
+
+    def getSynonymList(self, query):
+        '''
+        :param query: The query for which to get synonyms
+        :return: Synonyms in list format
+        '''
+        resultList = []
+        syns = self.getSynonyms(query)
+
+        charTermAttrib = syns.getAttribute(CharTermAttribute.class_)
+        syns.reset()
+
+        while syns.incrementToken():
+            resultList.append(charTermAttrib.toString())
+
+        return resultList
+
+if __name__ == '__main__':
+    text = "catcher, spatter"
+
+    # Use the default whitespaceanalyzer
+    analyser = WhitespaceAnalyzer()
+
+    # WordnetSynonymparser with whitespaceanalyzer
+    parser = WordnetSynonymParser(True, True, analyser)
+
+    # Read the prolog-file for wordnet in a stringreader
+    PlFile = StringReader(open("prolog/wn_s.pl", 'r').read())
+
+    # Parse the prologfile with the WordnetSynonymParser
+    parser.parse(PlFile)
+
+    # Build the synonymmap
+    map = parser.build()
+
+    tokenizer = StandardTokenizer()
+    tokenizer.setReader(StringReader(text))
+    a = SynonymGraphFilter(tokenizer, map, True)
+    a = FlattenGraphFilter(a)
+
+    charTermAttrib = a.getAttribute(CharTermAttribute.class_)
+    a.reset()
+
+    while a.incrementToken():
+        print(charTermAttrib)
